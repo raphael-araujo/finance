8bdf3d5274e23cd5c4fb4c5a8abc97ca0acb729f
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-07-04 19:53:01 -0300

    Configurando crispy_forms

--- core/settings.py
+++ core/settings.py
@@ -40,9 +40,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    # "apps":
+    # apps:
     "perfil",
+    # 3rd apps:
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
