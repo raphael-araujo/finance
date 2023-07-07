e79065a0d5001bc2f3fd59361c81a2119875f9b3
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-07-06 21:30:27 -0300

    Adicionando página para gerenciar contas

--- perfil/urls.py
+++ perfil/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('gerenciar_contas/', views.gerenciar_contas, name='gerenciar_contas'),
+    path('deletar_banco/<int:id>/', views.deletar_banco, name='deletar_banco'),
+    path('cadastrar_categoria/', views.cadastrar_categoria, name='cadastrar_categoria'),
+    path('atualizar_categoria/<int:id>/', views.atualizar_categoria, name='atualizar_categoria'),
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,7 +1,75 @@
+from django.contrib import messages
+
+# from django.db.models import Sum
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .forms import CategoriaForm, ContaForm
+from .models import Categoria, Conta
+from .utils import calcula_total
 
 # Create your views here.
 
+
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, 'home.html')
+    contas = Conta.objects.all()
+    saldo_total = calcula_total(contas, "valor")
+    print(saldo_total)
+    context = {
+        "contas": contas,
+        "saldo_total": saldo_total,
+    }
+
+    return render(request, "home.html", context)
+
+
+def gerenciar_contas(request: HttpRequest) -> HttpResponse:
+    contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
+    conta_form = ContaForm()
+    categoria_form = CategoriaForm
+
+    if request.method == "POST":
+        conta_form = ContaForm(request.POST, request.FILES)
+        if conta_form.is_valid():
+            conta_form.save()
+            messages.success(request, "Conta adicionada com sucesso.")
+            return redirect(to="gerenciar_contas")
+
+    valor_total = 0
+    for conta in contas:
+        valor_total += conta.valor
+
+    context = {
+        "contas": contas,
+        "categorias": categorias,
+        "conta_form": conta_form,
+        "categoria_form": categoria_form,
+        "valor_total": valor_total,
+    }
+    return render(request, "gerenciar_contas.html", context)
+
+
+def deletar_banco(request: HttpRequest, id: int) -> HttpResponse:
+    conta = get_object_or_404(Conta, id=id)
+    conta.delete()
+
+    messages.warning(request, "Conta removida com sucesso.")
+    return redirect(to="gerenciar_contas")
+
+
+def cadastrar_categoria(request: HttpRequest) -> HttpResponse:
+    categoria_form = CategoriaForm(request.POST)
+
+    if categoria_form.is_valid():
+        categoria_form.save()
+        messages.success(request, "Categoria cadastrada com sucesso.")
+    return redirect(to="gerenciar_contas")
+
+
+def atualizar_categoria(request: HttpRequest, id: int) -> HttpResponse:
+    categoria = get_object_or_404(Categoria, id=id)
+    categoria.essencial = not categoria.essencial
+    categoria.save()
+
+    return redirect(to="gerenciar_contas")
