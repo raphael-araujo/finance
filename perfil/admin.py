b213c3e5c99219792c0fb27c5737d9c6243af451
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-07-06 21:18:38 -0300

    Adicionando models Categoria e Conta

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Categoria, Conta
+
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Conta)
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    categoria = models.CharField(max_length=50)
+    essencial = models.BooleanField(default=False)
+    valor_planejado = models.DecimalField(max_digits=30, decimal_places=2, default=0)
+
+    def __str__(self) -> str:
+        return self.categoria
+
+
+class Conta(models.Model):
+    BANCO_CHOICES = (
+        ('NU', 'Nubank'),
+        ('CE', 'Caixa econômica'),
+        ('BB', 'Banco do Brasil'),
+        ('IT', 'Itaú'),
+    )
+    TIPO_CHOICES = (
+        ('PF', 'Pessoa Física'),
+        ('PJ', 'Pessoa Jurídica'),
+    )
+
+    apelido = models.CharField(max_length=50)
+    banco = models.CharField(max_length=2, choices=BANCO_CHOICES)
+    tipo = models.CharField(max_length=2, choices=TIPO_CHOICES)
+    valor = models.DecimalField(max_digits=30, decimal_places=2)
+    icone = models.ImageField(upload_to='icones')
+
+    def __str__(self) -> str:
+        return self.apelido
