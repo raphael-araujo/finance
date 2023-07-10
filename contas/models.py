5c7be7db985d894bfea67ecdd75245e9a2450950
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-07-10 17:28:52 -0300

    Adicionando models ContaPagar e ContaPaga

--- contas/models.py
+++ contas/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+from perfil.models import Categoria
+
+
+class ContaPagar(models.Model):
+    titulo = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    descricao = models.TextField()
+    valor = models.DecimalField(max_digits=30, decimal_places=2)
+    dia_de_pagamento = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.titulo
+
+
+class ContaPaga(models.Model):
+    conta = models.ForeignKey(ContaPagar, on_delete=models.DO_NOTHING)
+    data_pagamento = models.DateField()
