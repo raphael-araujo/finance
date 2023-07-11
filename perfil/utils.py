aec0f9292e1b2a88f398477d7d19f7be7b51c088
Raphael Araújo <raphaelaraujotoc@outlook.com>
2023-07-11 19:51:07 -0300

    Adicionando função para calcular equilibrio financeiro

--- perfil/utils.py
+++ perfil/utils.py
@@ -4,3 +4,24 @@ def calcula_total(instancia, campo):
         total += getattr(i, campo)
 
     return total
+
+
+def calcula_equilibrio_financeiro():
+    from datetime import datetime
+
+    from extrato.models import Extrato
+
+    gastos_essenciais = Extrato.objects.filter(data__month=datetime.now().month, tipo='S', categoria__essencial=True)
+    gastos_nao_essenciais = Extrato.objects.filter(data__month=datetime.now().month, tipo='S', categoria__essencial=False)
+
+    total_gastos_essenciais = calcula_total(gastos_essenciais, 'valor')
+    total_gastos_nao_essenciais = calcula_total(gastos_nao_essenciais, 'valor')
+
+    total = total_gastos_essenciais + total_gastos_nao_essenciais
+    try:
+        percentual_gastos_essenciais = total_gastos_essenciais * 100 / total
+        percentual_gastos_nao_essenciais = total_gastos_nao_essenciais * 100 / total
+
+        return percentual_gastos_essenciais, percentual_gastos_nao_essenciais
+    except:
+        return 0, 0
